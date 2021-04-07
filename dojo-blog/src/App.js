be919600ba39ff20861519aa9876c94854a61033
Jude Harding <jharding@genesisdla.com>
2021-04-07 17:21:11 -0400

    vid 5

--- dojo-blog/src/App.js
+++ dojo-blog/src/App.js
@@ -1,4 +1,6 @@
 import "./App.css";
+import Navbar from "./Navbar.js";
+import Home from "./Home.js";
 
 function App() {
 	// put in any regular js stuff BEFORE the return.
@@ -9,17 +11,29 @@ function App() {
 	// objects or booleans will not be interpolated
 	// const person = {name: 'yoshi', age: 30};
 
+	const link = "http://www.google.com";
+
 	return (
+		// TEMPLATE for the index.html includes the Navbar and App components
+
 		<div className="App">
+			{/* NAVBAR component  */}
+			<Navbar></Navbar>
+
+			{/* PAGE CONTENT */}
 			<div className="content">
 				<h1>App Component</h1>
+
 				<h2>{title}</h2>
+
+				<Home></Home>
 				<p>Liked {likes} times today.</p>
 				{/* <p>{person}</p> */}
 				<p>{10}</p>
 				<p>{"hello ninjas"}</p>
 				<p>{[1, 2, 3, 4, 5]}</p>
 				<p>{Math.random() * 10}</p>
+				<a href={link}>Google Site</a>
 			</div>
 		</div>
 	);
---
+++ dojo-blog/src/Navbar.js
@@ -0,0 +1,19 @@
+// import any files needed
+
+// type sfc and snippet will create a "stateless, functional component"
+// give it the name of your component and it will create a boilerplate
+const Navbar = () => {
+	return (
+		// returning jsx template for the NAVBAR
+		<nav className="navbar">
+			<h1>The Dojo Blog</h1>
+			<div className="links">
+				<a href="/">Home</a>
+				<a href="/create">New Blog</a>
+			</div>
+		</nav>
+	);
+};
+
+//exports here.  add import './Navbar.js on the App.js file
+export default Navbar;
